daf586cdcd770e31f44e1cbb10ce3e0df0dc3394
harshBlueFlag <somay.123468@gmail.com>
2024-09-18 03:29:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import DashboardPage from './pages/DashboardPage';
+import ClassManagementPage from './pages/ClassManagementPage';
+import TeacherProfilePage from './pages/TeacherProfilePage';
+import StudentProfilePage from './pages/StudentProfilePage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="layout">
+          <Sidebar />
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/classes" element={<ClassManagementPage />} />
+              <Route path="/teachers" element={<TeacherProfilePage />} />
+              <Route path="/students" element={<StudentProfilePage />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
